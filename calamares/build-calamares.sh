d97f76b51258f5e0ebc90e61894fa90d85c27665
arafsk <arafsos@protonmail.com>
2025-12-18 10:12:24 +0000

    update

---
+++ calamares/build-calamares.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#https://wiki.archlinux.org/index.php/DeveloperWiki:Building_in_a_Clean_Chroot
+
+destiny="$HOME/github/arafsk/araflinux/x86_64/"
+
+destiny=$destination1
+
+search1="calamares"
+search=$search1
+
+tput setaf 2
+echo "#############################################################################################"
+echo "#########        Let us build the package "$(basename `pwd`)
+echo "#############################################################################################"
+tput sgr0
+
+updpkgsums
+
+makepkg -s
+
+echo "Moving created files to " $destiny
+echo "#############################################################################################"
+mv $search*pkg.tar.zst $destiny
+
+echo "Cleaning up"
+echo "#############################################################################################"
+echo "deleting unnecessary folders"
+echo "#############################################################################################"
+
+rm -rf pkg src
+
+echo "deleting unnecessary files"
+echo "#############################################################################################"
+
+rm -rf calamares
+
+tput setaf 8
+echo "#############################################################################################"
+echo "###################                       build done                   ######################"
+echo "#############################################################################################"
+tput sgr0
