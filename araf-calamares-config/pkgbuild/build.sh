7734ba7c9f054438ce375ba00f84ec90b2a95aae
arafsk <arafsos@protonmail.com>
2026-01-02 19:29:57 +0000

    update

--- araf-calamares-config/pkgbuild/build.sh
+++ araf-calamares-config/pkgbuild/build.sh
@@ -7,7 +7,7 @@ arch=$(grep "^arch=" PKGBUILD | awk -F"'" '{print $2}')
 
 #NEED ONLY TO EDIT  sourcefiles VARIABLE
 
-sourcefiles="etc usr"
+sourcefiles="araf-calamares-config"
 
 #sed -i -e '/^sha256/d' -e '/^sha512/d' PKGBUILD
 
