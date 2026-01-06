1b124a6fe50a44bfac7df25d58d0433f89495804
arafsk <arafsos@protonmail.com>
2026-01-06 18:09:48 +0600

    update

--- mpv-git/build.sh
+++ mpv-git/build.sh
@@ -7,7 +7,7 @@ arch=$(grep "^arch=" PKGBUILD | awk -F"'" '{print $2}')
 
 #NEED ONLY TO EDIT  sourcefiles VARIABLE
 
-sourcefiles="araf-arc-dawn"
+sourcefiles="etc usr"
 
 #sed -i -e '/^sha256/d' -e '/^sha512/d' PKGBUILD
 
